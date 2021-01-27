002f582b568822d2a20b6094cdf8461ce75f4584
Alireza Farshin <farshin@kth.se>
2021-01-27 15:26:27 +0100

    results added

---
+++ js/plot.js
@@ -0,0 +1,119 @@
+function makeChart(packetApps) {
+
+  var appSize = packetApps.map(function(d) {
+    return d.Size;
+  });
+  var appPacketmill = packetApps.map(function(d) {
+    return +d.PacketMill/1000000000;
+  });
+
+  var appFastClick = packetApps.map(function(d) {
+    return +d.FastClick_CPY/1000000000;
+  });
+
+  var appVPP = packetApps.map(function(d) {
+    return +d.VPP/1000000000;
+  });
+
+   var appFastClick_Light = packetApps.map(function(d) {
+    return +d.FastClick_OVRLY/1000000000;
+  });
+
+   var appBESS = packetApps.map(function(d) {
+    return +d.BESS/1000000000;
+  });
+
+
+
+  var chart = new Chart('comparison-result-canvas', {
+    type: "line",
+	options: {
+		responsive: true,
+		title: {
+			display: true,
+			text: 'PacketMill vs. Other Packet Processing Frameworks'
+		},
+		tooltips: {
+			mode: 'index',
+			intersect: false,
+		},
+		hover: {
+			mode: 'nearest',
+			intersect: true
+		},
+		scales: {
+			xAxes: [{
+				display: true,
+				scaleLabel: {
+					display: true,
+					labelString: 'Packet Size (Byte)'
+				},
+			}],
+			yAxes: [{
+				display: true,
+				scaleLabel: {
+					display: true,
+					labelString: 'Throughput (Gbps)'
+				},
+				ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 100,
+                    beginAtZero: true
+                }
+			}]
+		}
+	}, 
+	data: {
+		labels: appSize,
+		datasets: [
+			{
+				label: 'PacketMill',
+				backgroundColor: window.chartColors.brewer_green2,
+				borderColor: window.chartColors.brewer_green2,
+				borderDash: [5, 1],
+				pointRadius: 4,
+				pointHoverRadius: 6,
+          		data: appPacketmill,
+          		fill: false,
+        	},
+			{
+				label: 'BESS',
+				backgroundColor: window.chartColors.brewer_green1,
+				borderColor: window.chartColors.brewer_green1,
+				borderDash: [3, 3],
+          		data: appBESS,
+          		fill: false,
+        	},  
+			{
+				label: 'FastClick (Light)',
+				backgroundColor: window.chartColors.brewer_blue2,
+				borderColor: window.chartColors.brewer_blue2,
+				borderDash: [10, 5],
+          		data: appFastClick_Light,
+          		fill: false,
+        	},  
+			{
+				label: 'FastClick',
+				backgroundColor: window.chartColors.brewer_blue1,
+				borderColor: window.chartColors.brewer_blue1,
+				borderDash: [5, 10],
+          		data: appFastClick,
+          		fill: false,
+        	},
+			{
+				label: 'VPP',
+				backgroundColor: window.chartColors.brewer_pink,
+				borderColor: window.chartColors.brewer_pink,
+				borderDash: [2, 2],
+          		data: appVPP,
+          		fill: false,
+        	},     	
+      	]
+    	}
+  });
+}
+
+// Request data using D3
+d3
+  .csv("/data/throughput.csv")
+  .then(makeChart);
---
+++ js/plot.utils.js
@@ -0,0 +1,16 @@
+'use strict';
+
+window.chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)',
+	brewer_blue1: 'rgb(166,206,227)',
+	brewer_blue2: 'rgb(31,120,180)',
+	brewer_green1: 'rgb(178,223,138)',
+	brewer_green2: 'rgb(51,160,44)',
+	brewer_pink: 'rgb(251,154,153)',
+};
